41c74b4640b292de4208150f74e2735369820bd0
Anna Lozytska <alozytska@cogniance.com>
2016-09-13 14:48:17 +0300

    Added skeleton of repository

---
+++ app/src/main/java/devchallenge/android/radiotplayer/repository/Storage.java
@@ -0,0 +1,50 @@
+package devchallenge.android.radiotplayer.repository;
+
+import com.yahoo.squidb.data.ISQLiteDatabase;
+import com.yahoo.squidb.data.ISQLiteOpenHelper;
+import com.yahoo.squidb.data.SquidDatabase;
+import com.yahoo.squidb.sql.Table;
+
+import devchallenge.android.radiotplayer.repository.modelspec.PodcastInfoRow;
+
+public class Storage extends SquidDatabase {
+    private static volatile Storage sInstance;
+    private static final String DB_NAME = "radio_t_player.db";
+    private static final int VERSION = 1;
+
+    public static Storage getInstance() {
+        synchronized (Storage.class) {
+            if (sInstance == null) {
+                sInstance = new Storage();
+            }
+        }
+        return sInstance;
+    }
+
+    @Override
+    public String getName() {
+        return DB_NAME;
+    }
+
+    @Override
+    protected int getVersion() {
+        return VERSION;
+    }
+
+    @Override
+    protected Table[] getTables() {
+        return new Table[] {
+                PodcastInfoRow.TABLE
+        };
+    }
+
+    @Override
+    protected boolean onUpgrade(ISQLiteDatabase db, int oldVersion, int newVersion) {
+        return false;
+    }
+
+    @Override
+    protected ISQLiteOpenHelper createOpenHelper(String databaseName, OpenHelperDelegate delegate, int version) {
+        return null;
+    }
+}
---
+++ app/src/main/java/devchallenge/android/radiotplayer/repository/modelspec/PodcastInfoSpec.java
@@ -0,0 +1,7 @@
+package devchallenge.android.radiotplayer.repository.modelspec;
+
+import com.yahoo.squidb.annotations.TableModelSpec;
+
+@TableModelSpec(className="PodcastInfoRow", tableName="podcastsInfo")
+public class PodcastInfoSpec {
+}
