3b07a802fecc0b5897d50238d8c62d21bf9b522a
Anna Lozytska <alozytska@cogniance.com>
2016-09-16 09:36:04 +0300

    Test implementation of MadiaPlayer

--- app/src/main/java/devchallenge/android/radiotplayer/ui/activity/MainActivity.java
+++ app/src/main/java/devchallenge/android/radiotplayer/ui/activity/MainActivity.java
@@ -1,24 +1,38 @@
 package devchallenge.android.radiotplayer.ui.activity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.squareup.otto.Subscribe;
 
+import java.io.IOException;
 import java.util.EventListener;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import devchallenge.android.radiotplayer.R;
 import devchallenge.android.radiotplayer.event.EventManager;
+import devchallenge.android.radiotplayer.event.PodcastsLoadedEvent;
 import devchallenge.android.radiotplayer.event.PodcastsSyncEvent;
+import devchallenge.android.radiotplayer.net.model.PodcastItem;
 import devchallenge.android.radiotplayer.repository.PodcastsInfoProvider;
 
 public class MainActivity extends AppCompatActivity implements EventListener {
+
+    //TODO: DELETE AFTER TESTING:
+    private TextView url;
+    private Button stop;
+    private MediaPlayer player;
 
     private ImageButton mSync;
     private View.OnClickListener mStartSyncListener = new View.OnClickListener() {
@@ -39,6 +53,15 @@ public class MainActivity extends AppCompatActivity implements EventListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        url = (TextView) findViewById(R.id.playing_url);
+        stop = (Button) findViewById(R.id.stop);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player.stop();
+            }
+        });
+        player = new MediaPlayer();
     }
 
     @Override
@@ -62,6 +85,29 @@ public class MainActivity extends AppCompatActivity implements EventListener {
         EventManager.getInstance().unregisterEventListener(this);
     }
 
+    //TODO: DELETE AFTER TESTING:
+    @Subscribe
+    public void onPodcLoaded(PodcastsLoadedEvent event) {
+        List<PodcastItem> podcasts = event.getPodcasts();
+        String audioUrl = podcasts.get(0).getEnclosureUrl();
+        url.setText(audioUrl);
+        Uri audioUri = Uri.parse(audioUrl);
+        try {
+            //TODO warn user if network speed is low for streaming
+            player.setDataSource(this, audioUri);
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            player.prepareAsync();
+            player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    player.start();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Subscribe
     public void onSyncUpdates(PodcastsSyncEvent event) {
         if (mSync != null) {
@@ -76,7 +122,7 @@ public class MainActivity extends AppCompatActivity implements EventListener {
                 case FINISHED:
                     boolean isRecent =
                             (System.currentTimeMillis() - event.getEventTimestamp())
-                                    <= TimeUnit.MINUTES.toMillis(10);
+                                    <= TimeUnit.MINUTES.toMillis(5);
                     mSync.clearAnimation();
                     mSync.setImageResource(isRecent ? R.drawable.ic_done_white_24dp
                             : R.drawable.ic_sync_white_24dp);
